4390793ecb3329f9a10719c43deec2ed322084da
HarikiranPaili <harikiran040@gmail.com>
2022-02-16 16:19:07 +0530

    first commit

--- store/settings.py
+++ store/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x6w=($)c$6%(c80-tbdr+hg)*ch*ginhd2wjtwsjp+dn+3s#7$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['freshvegs.herokuapp.com', 'localhost', '127.0.0.1']
@@ -119,11 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+DEBUG = True
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles',)
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
